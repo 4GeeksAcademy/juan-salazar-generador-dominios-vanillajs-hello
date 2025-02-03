eb0a09e9cc1439ef4337a92ac95560acc2a951c0
JRSalazar2598 <155923860+JRSalazar2598@users.noreply.github.com>
2025-02-02 20:39:34 -0600

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dom = [".com"];
+
+  let generator = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < dom.length; l++) {
+          let mixing = pronoun[i] + adj[j] + noun[k] + dom[l];
+          generator.push(mixing);
+        }
+      }
+    }
+  }
+
+  console.log(generator);
+
   console.log("Hello Rigo from the console!");
 };
